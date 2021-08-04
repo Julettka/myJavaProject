fb69d01eb2a7d60d47a8423bfaa4596b2dfd4960
Юлия Васильевна <Jul.ettka@gmail.com>
2021-08-04 16:17:25 +0300

    First commit

---
+++ javaLessons1/src/main/java/com/andersenlab/vorobei/school/lesson3/Sotrudnik.java
@@ -0,0 +1,51 @@
+package com.andersenlab.vorobei.school.lesson3;
+
+public class Sotrudnik {
+    public static void main(String[] args) {
+        vozrastZa40();
+    }
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Sotrudnik(String fullName, String position, String email, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void info() {
+        System.out.println("FullName is " + fullName);
+        System.out.println("Position is " + position);
+        System.out.println("Email is " + email);
+        System.out.println("Phone Number is " + phoneNumber);
+        System.out.println("Salary is " + salary);
+        System.out.println("Age is " + age);
+        System.out.println();
+    }
+
+    public static void vozrastZa40() {
+        Sotrudnik[] sotrArray = new Sotrudnik[5];
+        sotrArray[0] = new Sotrudnik("Petr Ivanov", "manager", "o.ppp@gmail.com", "298168348", 5000, 45);
+        sotrArray[1] = new Sotrudnik("Petr Petrov", "director", "o.ddd@gmail.com", "298458345", 6000, 40);
+        sotrArray[2] = new Sotrudnik("Petr Suhov", "manager", "a.uuu@gmail.com", "299668258", 7000, 29);
+        sotrArray[3] = new Sotrudnik("Petr Fandorin", "director", "s.lll@gmail.com", "293668878", 8000, 55);
+        sotrArray[4] = new Sotrudnik("Petr Muhov", "manager", "k.www@gmail.com", "298998336", 9000, 23);
+        for (int i = 0; i < sotrArray.length; i++) {
+            if (sotrArray[i].getAge() > 40) {
+                sotrArray[i].info();
+            }
+        }
+    }
+}
